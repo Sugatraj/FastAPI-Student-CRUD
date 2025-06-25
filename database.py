f4288f18f984e56aadfa6d4edda7c8d36e793762
Sugatraj <sugatraj.2106@gmail.com>
2025-06-25 10:54:30 +0530

    Implement CRUD operations for students and update database connection handling

--- database.py
+++ database.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 URL_DATABASE = 'mysql+pymysql://root:SugaSensei%402025@localhost:3306/students'
+pool_pre_ping=True #use this for if mysql goes down and this will auto connect to mysql
 
 engine = create_engine(URL_DATABASE)
 
--- main.py
+++ main.py
@@ -2,10 +2,52 @@ from fastapi import FastAPI, HTTPException, Depends, status
 from pydantic import BaseModel
 from typing import Annotated
 import models
-# from database import engine, SessionLocal
+from database import engine, SessionLocal
+from sqlalchemy.orm import Session
 
 app = FastAPI()
+
+models.Base.metadata.create_all(bind=engine)
+
+class StudentBase(BaseModel):
+    name : str
+
+class UpdateStudentBase(BaseModel):
+    name: str
+
+def get_db():
+    db =  SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
 
 @app.get('/', status_code=status.HTTP_200_OK)
 async def welcome():
     return {'welcome to student crud fast api appliaction'}
+
+@app.post('/student', status_code=status.HTTP_201_CREATED)
+async def create_student(student:StudentBase, db:db_dependency):
+    db_student = models.Student(**student.dict())
+    db.add(db_student)
+    db.commit()
+    return {'message' : "studnet created successfully"}
+
+@app.get('/students/{id}', status_code=status.HTTP_200_OK)
+async def read_students(id: int, db: db_dependency):
+    student = db.query(models.Student).filter(models.Student.id == id).first()
+    if student is None:
+        raise HTTPException(status_code=404, detail='student not found')
+    return student
+
+@app.patch('/students/{id}', status_code=status.HTTP_200_OK)
+async def update_student(id: int, student_update: UpdateStudentBase, db: db_dependency):
+    db_student = db.query(models.Student).filter(models.Student.id == id).first()
+
+    if db_student is None :
+        raise HTTPException(status_code=404, detail='student not found')
+    
+    update_data = student_update.dict
